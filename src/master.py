926ff5b91b52e17b0c0200f3ce7018adf28ac156
James Dziewulski-Miles <j.dziewulskimiles@gmail.com>
2018-09-13 22:16:56 +0100

    Moved key to local env and updated gitignore

--- src/master.py
+++ src/master.py
@@ -5,7 +5,7 @@
 import os
 import requests
 
-API_KEY = "K3C250DN2IWQWVBL" # TODO: Store in ENV
+API_KEY = os.environ["API_KEY"]
 BASE_URL = "https://www.alphavantage.co/query?" # TODO: Take URL params as separate inputs (gui, web?)
 REQUEST_URL = "https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=USD&to_symbol=JPY&interval=5min"
 
