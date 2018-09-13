3c75e3d6f42291b719248d660b49c8b74e76d30a
James Dziewulski-Miles <j.dziewulskimiles@gmail.com>
2018-09-13 22:10:05 +0100

    Initial info added to master.py

---
+++ src/master.py
@@ -0,0 +1,18 @@
+# Python Script focusing on price action and
+# technical analysis for FX and Cryptos (or any asset)
+# @author James Miles, Github: jamesdzm
+
+import os
+import requests
+
+API_KEY = "K3C250DN2IWQWVBL" # TODO: Store in ENV
+BASE_URL = "https://www.alphavantage.co/query?" # TODO: Take URL params as separate inputs (gui, web?)
+REQUEST_URL = "https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=USD&to_symbol=JPY&interval=5min"
+
+def get_price_data(url):
+    # GET REQUEST_URL
+    price_data = ""
+    return price_data
+
+def analyse_price_data(price_data):
+    return None
